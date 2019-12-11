0994f747ef0424eec41d3a7a88c42b1a8c919d9c
Bito Mann <bito@bitomann.com>
2019-12-11 10:43:11 -0600

    oops

--- keto-bito/src/components/nav/NavBar.js
+++ keto-bito/src/components/nav/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
+import './NavBar.css'
+// import "bootstrap/dist/css/bootstrap.min.css"
 
 
 class NavBar extends Component {
@@ -9,28 +10,15 @@ class NavBar extends Component {
             <nav className="navbar bg-dark text-white flex-md-nowrap p-0 shadow">
                 <ul className="nav nav-pills nav-fill">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/articles">Articles</Link>
+                        <Link className="nav-link" to="/articles">SHOPPING LIST</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/friends">Friends</Link>
+                        <Link className="nav-link" to="/friends">FOODS</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/messages">Messages</Link>
+                        <Link className="nav-link" to="/messages">LOGIN</Link>
                     </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/tasks">Tasks</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/events">Events</Link>
-                    </li>
-                </ul>
-                <span className="navbar-text">
-                    <ul className="nav nav-pills nav-fill">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
                     </ul>
-                </span>
             </nav>
         )
     }
